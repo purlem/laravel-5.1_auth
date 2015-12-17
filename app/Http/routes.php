69a80223965a060983dd9e5a6bec50c9e17de55b
Marty Thomas <mthomas@purlem.com>
2015-12-17 13:08:05 -0600

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,8 +21,8 @@ Route::post('login', 'Auth\AuthController@postLogin');
 Route::get('logout', 'Auth\AuthController@getLogout');
 
 // Registration routes...
-Route::get('register', 'Auth\AuthController@getRegister');
-Route::post('register', 'Auth\AuthController@postRegister');
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 // Password reset link request routes...
 Route::get('password/email', 'Auth\PasswordController@getEmail');
